8b8d291627655916e9b143e82df2d57e14ca3b8f
miahela <mihaela.pavleska@gmail.com>
2024-05-21 04:34:24 +0000

    Updated database connection configuration and added test script

---
+++ test-db-connection.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PASS:', typeof process.env.DB_PASS, process.env.DB_PASS);
+console.log('DB_PORT:', process.env.DB_PORT);
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }
+});
+
+pool.connect((err, client, release) => {
+    if (err) {
+        console.error('Error acquiring client', err.stack);
+    } else {
+        console.log('Database connection successful');
+    }
+    release();
+    pool.end();
+});
