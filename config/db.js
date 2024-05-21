6c99b161ac43c10b6b74a8b137f55e35a8246743
Mihaela Pavleska <mihaela.pavleska@gmail.com>
2024-05-21 06:16:41 +0200

    Fixing database connection

--- config/db.js
+++ config/db.js
@@ -3,6 +3,12 @@ const {
 } = require('pg');
 require('dotenv').config();
 
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PASS:', typeof process.env.DB_PASS);
+console.log('DB_PORT:', process.env.DB_PORT);
+
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
@@ -18,9 +24,5 @@ pool.on('connect', () => {
     console.log('Connected to the DB');
 });
 
-pool.on('error', (err) => {
-    console.error('Unexpected error on idle client', err);
-    process.exit(-1);
-});
 
 module.exports = pool;
