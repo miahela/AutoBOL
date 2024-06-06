d8290d8a8cfeed5bc58c4c4d8c56a27f4686f768
Mihaela Pavleska <mihaela.pavleska@gmail.com>
2024-06-06 03:57:56 +0200

    Fixed delete old entries script

--- tools/monday_api/delete_old_entries.js
+++ tools/monday_api/delete_old_entries.js
@@ -9,40 +9,43 @@ const CANADA_BOARD_ID = process.env.CA_BOARD_ID;
 monday.setToken(MONDAY_API_KEY);
 
 async function deleteAllItems(boardId) {
-    let page = 1;
     let items = [];
+    let hasMoreItems = true;
+
     try {
-        do {
-            const query = `query($boardId: [Int], $page: Int) {
-                boards(ids: $boardId) {
-                    items(limit: 25, page: $page) {
-                        id
-                        name
+        while (hasMoreItems) {
+            const query = `query($boardId: ID!, $limit: Int) {
+                boards(ids: [$boardId]) {
+                    items_page(limit: $limit) {
+                        items {
+                            id
+                            name
+                        }
                     }
                 }
             }`;
 
             const variables = {
-                boardId: [parseInt(boardId)],
-                page
+                boardId: boardId,
+                limit: 100,
             };
             const res = await monday.api(query, {
                 variables
             });
 
-            console.log(res);
+            console.log("Response:", res); // Debugging response
 
             if (res.data && res.data.boards.length > 0) {
-                items = res.data.boards[0].items;
+                items = res.data.boards[0].items_page.items;
                 for (let item of items) {
                     await deleteItem(item.id);
                 }
+                hasMoreItems = items.length === 25;
             } else {
                 console.log(`No items found on board ${boardId}`);
-                items = [];
+                hasMoreItems = false;
             }
-            page++;
-        } while (items.length > 0);
+        }
     } catch (err) {
         console.error(`Error fetching items from board ${boardId}:`, err);
     }
@@ -50,14 +53,14 @@ async function deleteAllItems(boardId) {
 
 async function deleteItem(itemId) {
     try {
-        const query = `mutation($itemId: Int!) {
-            delete_item (item_id: $itemId) {
+        const query = `mutation($itemId: ID!) {
+            delete_item(item_id: $itemId) {
                 id
             }
         }`;
 
         const variables = {
-            itemId: parseInt(itemId)
+            itemId: itemId
         };
         const res = await monday.api(query, {
             variables
